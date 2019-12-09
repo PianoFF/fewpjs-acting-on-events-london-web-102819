002d46f134d5829bf29684fbd88968239b5275d0
PianoFF <Fangfei1star@gmail.com>
2019-12-09 19:46:44 +0000

    with up and down

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function moveDodgerLeft(){
     let left= parseInt(leftNumbers, 10);
     
     if (left>0){
-        dodger.style.left=`${left-1}px`;
+        dodger.style.left=`${left-5}px`;
     }
 }
 
@@ -16,7 +16,26 @@ function moveDodgerRight(){
     let left=parseInt(leftNumbers, 10);
 
     if (left<360){
-        dodger.style.left=`${left+1}px`;
+        dodger.style.left=`${left+5}px`;
+    }
+}
+
+function moveDodgerUp(){
+    let upNumbers=dodger.style.bottom.replace("px", "");
+    let up=parseInt(upNumbers, 10);
+
+    if (up<379){
+        dodger.style.bottom=`${up+5}px`;
+    }
+}
+
+
+function moveDodgerDown(){
+    let upNumbers=dodger.style.bottom.replace("px", "");
+    let up=parseInt(upNumbers, 10);
+
+    if (up>0){
+        dodger.style.bottom=`${up-5}px`;
     }
 }
 
@@ -27,6 +46,18 @@ document.addEventListener("keydown", function(e) {
     else if(e.key==="ArrowRight"){
         moveDodgerRight();
     }
+    else if(e.key==="ArrowUp"){
+        moveDodgerUp();
+    }
+    else if(e.key==="ArrowDown"){
+        moveDodgerDown();
+    }
   });
   
+  /* type this in the devTools Console, and see what key you are pressing 
+  document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
+*/
+
   
